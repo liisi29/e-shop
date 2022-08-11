efea326cfe3e35e61d8115cfb8a1fb556e505ad5
liisi.raidaru <liisi.raidaru@turnit.com>
2022-08-12 02:56:30 +0300

    Refactored navigation

--- src/routes/nav/navigation.tsx
+++ src/routes/nav/navigation.tsx
@@ -3,10 +3,12 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase.utils';
 import { UserContext } from '../../contexts/user.context';
-import './navigation.styles.scss';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import { CartContext } from '../../contexts/cart.context';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
+import './navigation.scss';
+import TRANS from '../../translations/en.json';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -19,16 +21,16 @@ const Navigation = () => {
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>
-            SHOP
+            {TRANS.nav.shop}
           </Link>
 
           {currentUser ? (
             <span className='nav-link' onClick={signOutUser}>
-              SIGN OUT
+              {TRANS.nav.signOut}
             </span>
           ) : (
             <Link className='nav-link' to='/auth'>
-              SIGN IN
+              {TRANS.nav.signIn}
             </Link>
           )}
           <CartIcon />
